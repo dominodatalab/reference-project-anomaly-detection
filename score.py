d5463207cd71596c7fe9b5ad539e48ff35ebe0aa
Subir Mansukhani <subir.mansukhani@dominodatalab.com>
2023-10-12 17:14:18 -0700

    store and read model from /mnt/artifacts
    Pushed from Domino: https://demo2.dominodatalab.com/workspace/subir_mansukhani/anamoly?executionId=6528813935d1aa399f3d4e9a

--- score.py
+++ score.py
@@ -3,9 +3,10 @@ import sys
 
 from anomalib.deploy import OpenVINOInferencer
 
+# Please change these paths to match where you have stored the model and make sure all workspace artifacts have been syned before starting the model API
 inferencer = OpenVINOInferencer(
-    path="padim/mvtec/mteal_nut/run/weights/openvino/model.bin",
-    metadata="padim/mvtec/mteal_nut/run/weights/openvino/metadata.json",  
+    path="/mnt/artifacts/padim/mvtec/mteal_nut/run/weights/openvino/model.bin",
+    metadata="/mnt/artifacts/padim/mvtec/mteal_nut/run/weights/openvino/metadata.json",  
     device="CPU"
 )
 
@@ -14,6 +15,7 @@ def score(image):
     predictions = inferencer.predict(image=image)
     return predictions.pred_score, predictions.pred_label
 
+# This won't be executed when the model API is run/setup. Also note that Domino datasets are not accessible from Model APIs
 if __name__ == "__main__":
     dataset_path = os.path.join(os.environ["DOMINO_DATASETS_DIR"], os.environ["DOMINO_PROJECT_NAME"])
     pred_score, pred_label = score(os.path.join(dataset_path, "metal_nut/test/bent/000.png"))
